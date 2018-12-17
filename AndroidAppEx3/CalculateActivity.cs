312c4e4aeb49c0ff6887a78070bea607d2171ba8
Özgür ÖZŞEN <oozsen@anadolu.edu.tr>
2018-12-17 16:45:16 +0300

    .

--- AndroidAppEx3/CalculateActivity.cs
+++ AndroidAppEx3/CalculateActivity.cs
@@ -15,12 +15,33 @@ namespace AndroidAppEx3
     [Activity(Label = "CalculateActivity")]
     public class CalculateActivity : Activity
     {
+        TextView txtResult;
+        Button btnDone;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.Calculate);
+
+            txtResult = FindViewById<TextView>(Resource.Id.txtResult);
+            btnDone = FindViewById<Button>(Resource.Id.btnDone);
+            
+            Intent myIntent = new Intent();
+            int val1 = int.Parse(Intent.GetStringExtra("val1"));
+            int val2 = int.Parse(Intent.GetStringExtra("val2"));
+
+            int vresult = val1 + val2;
+            txtResult.Text = "Result is " + vresult;
+            myIntent.PutExtra("vresult", vresult);
+            SetResult(Result.Ok, myIntent);
+
+            btnDone.Click += BtnDone_Click;            
+        }
+
+        private void BtnDone_Click(object sender, EventArgs e)
+        {
+            Finish();
         }
     }
 }
--- AndroidAppEx3/MainActivity.cs
+++ AndroidAppEx3/MainActivity.cs
@@ -2,6 +2,8 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Content;
+using Android.Runtime;
 
 namespace AndroidAppEx3
 {
@@ -29,8 +31,21 @@ namespace AndroidAppEx3
 
         private void BtnAdd_Click(object sender, System.EventArgs e)
         {
-            Toast.MakeText(this, txtVal1.Text + " + " + txtVal2.Text, ToastLength.Short).Show();
-            txtSum.Text = "Calculating";
+            //Toast.MakeText(this, txtVal1.Text + " + " + txtVal2.Text, ToastLength.Short).Show();
+            txtSum.Text = "Calculating...";
+
+            var intent = new Intent(this, typeof(CalculateActivity));
+            intent.PutExtra("val1", txtVal1.Text);
+            intent.PutExtra("val2", txtVal2.Text);
+            StartActivityForResult(intent, 493);
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            if (resultCode == Result.Ok && requestCode == 493) {                
+                int result = data.GetIntExtra("vresult", -1);
+                txtSum.Text = " Sum is " + result;
+            }
         }
     }
 }
